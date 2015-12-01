3b821d04a9b57c24694f4fd5d392387344abc474
sylgas <sylwia.m.gasior@gmail.com>
2015-12-01 17:16:12 +0100

    Remove unnecessary log

--- frontend/public/js/BattleshipService.js
+++ frontend/public/js/BattleshipService.js
@@ -61,7 +61,6 @@ service('BattleshipService', ['_', 'LoggedUser', function(_, LoggedUser) {
     });
 
     this.createGame = function(gameName, maxPlayers, callback) {
-        console.log("here00");
         gameCreationCallbacks[gameName] = callback;
         socket.emit('create_game', {
             gameName: gameName,
